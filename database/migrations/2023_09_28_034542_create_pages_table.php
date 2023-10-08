016e283cbb32381bcd773a41e56fd8e2cca68e3c
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-10-08 15:20:16 +0600

    Day-03 - API Database Migrations+Models

--- database/migrations/2023_09_28_034542_create_pages_table.php
+++ database/migrations/2023_09_28_034542_create_pages_table.php
@@ -10,14 +10,11 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('shop_id')->nullable()->comment('Shop ID of the user');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pages');
     }
 };
--- database/migrations/2023_09_28_034555_create_product_images_table.php
+++ database/migrations/2023_09_28_034555_create_product_images_table.php
@@ -10,14 +10,12 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('shop_id')->nullable()->comment('Shop ID of the user');
-            $table->rememberToken();
+            $table->unsignedBigInteger('product_id');
+            $table->string('file');
+            $table->boolean('is_featured')->default(false);
+            $table->unsignedInteger('order')->default(10);
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_images');
     }
 };
