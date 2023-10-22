cc57158ad40f9425d38e56fa4cfcf74af0f92054
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-10-23 00:07:14 +0600

    Day 04 - Design Pattern and API architecture

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CategorySeeder::class,
             RolePermissionSeeder::class,
             UserSeeder::class,
         ]);
